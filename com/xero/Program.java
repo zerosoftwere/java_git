30516c7bae6e15aa18c154ecfc3bc20e22a0ba3a
xero <zerosoftwere@gmail.com>
2014-11-25 15:35:25 +0000

    Add reculsiveFactoria test case to main method

--- com/xero/Program.java
+++ com/xero/Program.java
@@ -5,6 +5,7 @@ class Program
 	public static void main(String[] args)
 	{
 		System.out.println(iterativeFactoria(5));
+		System.out.println(reculsiveFactoria(5));
 	}
 	
 	public static int iterativeFactoria(int n)
