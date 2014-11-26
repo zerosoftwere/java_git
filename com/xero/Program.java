88de91de88854819bbf7e1fa0be27ade5899c313
xero <zerosoftwere@gmail.com>
2014-11-26 12:28:48 +0000

    Removed factoria methods from Program class and updated test case

--- com/xero/Program.java
+++ com/xero/Program.java
@@ -4,20 +4,6 @@ class Program
 {
 	public static void main(String[] args)
 	{
-		System.out.println(reculsiveFactoria(5));
-	}
-	
-	public static int iterativeFactoria(int n)
-	{
-		int result = 1;
-		for (int i = n; i > 1; i--)
-			result *= i;
-		return result;
-	}
-	
-	public static int reculsiveFactoria(int n)
-	{
-		if (n < 1) return 1;
-		return n * reculsiveFactoria(n - 1);
+		System.out.println(Factoria.reculsiveFactoria(5));
 	}
 }
